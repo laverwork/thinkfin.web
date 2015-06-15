e3a828ce258f8b9a2a44191774c60047726bcb76
Matt <mattlaver@gmail.com>
2015-06-15 23:42:14 +0100

    Initial project

---
+++ thinkfin.web/thinkfin.web/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace thinkfin.web.Controllers
+{
+    public sealed class AccountController : Controller
+    {
+        [Authorize]
+        public ActionResult SignIn()
+        {
+            return this.Redirect("/");
+        }
+
+        public ActionResult SignOut()
+        {
+            this.Request.GetOwinContext().Authentication.SignOut();
+            return this.Redirect("/");
+        }
+    }
+}
---
+++ thinkfin.web/thinkfin.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="thinkfin.web.MvcApplication" Language="C#" %>
