7f2cadedd6fa57acc796a13d5da516a3f185941a
Matt <mattlaver@gmail.com>
2015-06-17 21:12:06 +0100

    Add authorize to HomeController

--- thinkfin.web/thinkfin.web/Controllers/HomeController.cs
+++ thinkfin.web/thinkfin.web/Controllers/HomeController.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace thinkfin.web.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +18,7 @@ namespace thinkfin.web.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
