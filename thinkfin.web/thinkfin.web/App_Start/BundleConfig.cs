f7d9918a7ef0d92649e3a5a2a8da45eb3308e3d2
Matt <mattlaver@gmail.com>
2015-06-17 22:36:37 +0100

    Added some style

--- thinkfin.web/thinkfin.web/App_Start/BundleConfig.cs
+++ thinkfin.web/thinkfin.web/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace thinkfin.web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.slimscroll.js",
+                        "~/Scripts/metismenu.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,13 +21,16 @@ namespace thinkfin.web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/inspinia.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/font-awesome.css",
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/style.css"));
         }
     }
 }
